35af3a5dab3782d0317fb0cba4d647df11217d03
julialee64 <julialee64@131.229.41.229>
2019-02-14 16:48:11 -0500

    end of lab

--- src/main/java/edu/smith/cs/csc212/p4/Smith.java
+++ src/main/java/edu/smith/cs/csc212/p4/Smith.java
@@ -1,5 +1,12 @@
 package edu.smith.cs.csc212.p4;
-
-public class Smith {
-
+import java.util.HashMap;
+public class Smith implements GameWorld {
+	private Map<String, Place> places = new HashMap<>();
+	public String getStart() {
+		return "GrecourtGates ";
+		
+	}
+		
 }
+	
+	
